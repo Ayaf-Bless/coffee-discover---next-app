9b791ad06d083db2d20fb780d59b6d90a6b70ce3
ayaf-bless <blessambel1@gmail.com>
2021-12-05 19:44:44 +0300

    refactore coffee store

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,10 +1,50 @@
 import React from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
-function Index(props) {
+import styles from "../../styles/cofeeStore.module.css";
+import coffeeStore from "../../data/coffee-stores.json";
+
+function Index({ coffeeStore }) {
   const router = useRouter();
   const { id } = router.query;
-  return <div>coffee store number: {id}</div>;
+  if (router.isFallback) {
+    return <p>Loadings...</p>;
+  }
+  const { name, address } = coffeeStore;
+
+  return (
+    <div>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      coffee store number: {id}
+      <p>{address}</p>
+    </div>
+  );
+}
+export async function getStaticPaths() {
+  const paths = coffeeStore.map((el) => {
+    return {
+      params: {
+        id: el.id.toString(),
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: true,
+  };
+}
+export async function getStaticProps({ params }) {
+  console.log(params);
+  return {
+    props: {
+      coffeeStore: coffeeStore.find((el) => {
+        return el.id.toString() === params.id;
+      }),
+    }, // will be passed to the page component as props
+  };
 }
 
 export default Index;
